8e535b26bb0bf217c1732a709406f1f26e3cacb5
Mohamed Trigui <68602896+MotriTN@users.noreply.github.com>
2023-09-24 23:59:33 +0100

    Update Exercice-2.py

--- Exercice-2.py
+++ Exercice-2.py
@@ -3,10 +3,15 @@ while not (ch[-1]=="."):
     ch=input("Donner ch : ")
 k=" "
 s=""
+p=0
 for i in range(len(ch)):
     if ch[i]!=k:
         s+=ch[i]
     else:
         if i+1<len(ch) and ch[i+1]!=k:
             s+=k
+            p+=1
+if s[-2]!=k:
+    p+=1
+print("mots: ",p)
 print(s)
