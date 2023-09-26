e218788867e2d96efd12ee1ed34aee267dd1db5c
Mohamed Trigui <68602896+MotriTN@users.noreply.github.com>
2023-09-26 16:31:14 +0200

    Update Exercice-2.py

--- Exercice-2.py
+++ Exercice-2.py
@@ -1,17 +1,20 @@
-ch=input("Donner ch : ")
-while not (ch[-1]=="."):
-    ch=input("Donner ch : ")
-k=" "
-s=""
-p=0
+ch = input("Donner ch : ")
+while not ((ch[-1] == ".") and (ch != ".")):
+    ch = input("Donner ch : ")
+k = " "
+s = ""
+p = 0
 for i in range(len(ch)):
-    if ch[i]!=k:
-        s+=ch[i]
+    if ch[i] != k:
+        s += ch[i]
     else:
-        if i+1<len(ch) and ch[i+1]!=k:
-            s+=k
-            p+=1
-if s[-2]!=k:
-    p+=1
-print("mots: ",p)
+        if i + 1 < len(ch) and ch[i + 1] != k:
+            s += k
+            p += 1
+if (s[-2] != k):
+    p += 1
+else:
+    s = s[0:-2] + "."
+
+print("mots: ", p)
 print(s)
