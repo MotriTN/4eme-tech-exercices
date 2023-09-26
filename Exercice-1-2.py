11982d10f95345840612f20b01e39bdd118ec289
Mohamed Trigui <68602896+MotriTN@users.noreply.github.com>
2023-09-26 16:11:57 +0200

    Correction Exercice-1-2.py

--- Exercice-1-2.py
+++ Exercice-1-2.py
@@ -3,9 +3,9 @@ N=int(input(("Donner N : ")))
 while not (5<=N<=150):
     N=int(input(("Donner N : ")))
 s=1
-i=0
-for i in range (2,N+1,2):
-    s-=1/N**2
-for i in range (3,N+1,2):
-    s+=1/N**2
+for i in range (2,N+1):
+    if i % 2 == 0:
+        s -= 1 / i**2
+    else:
+        s += 1 / i**2
 print(s)
